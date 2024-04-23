5b4c1ddddda7166f4b02bf990daf3857b8696d59
Phạm Trần Anh Nhật <fopop2@gmail.com>
2024-04-23 21:06:01 +0700

    Fix status screen loading

--- app/src/main/java/com/nhom9/message/screens/subsettingscreens/accountsettingsubscreens/EditNameScreen.kt
+++ app/src/main/java/com/nhom9/message/screens/subsettingscreens/accountsettingsubscreens/EditNameScreen.kt
@@ -1,12 +1,18 @@
 package com.nhom9.message.screens.subsettingscreens.accountsettingsubscreens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.nhom9.message.CommonProgressbar
 import com.nhom9.message.MViewModel
@@ -27,11 +33,14 @@ fun EditNameScreen(navController: NavController, viewModel: MViewModel){
         val onButtonClick:()->Unit={
             viewModel.createOrUpdateProfile(name = name)
         }
-
         Column {
             TitleBarWithBackAndRightButton(text = "Edit Name", "Save", onButtonClick)
-            OutlinedTextField(value = name, onValueChange = {name = it})
+            Column(modifier  =Modifier.padding(8.dp)) {
 
+                OutlinedTextField(value = name, onValueChange = {name = it}, modifier = Modifier.fillMaxWidth())
+                Text(text = "Choose a good name for yourself!", color = Color.LightGray, modifier = Modifier.padding(top = 8.dp))
+            }
         }
+
     }
 }
